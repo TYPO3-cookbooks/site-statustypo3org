2e4b8c6986e3b747c61b876a6382443eb8e874b4
Steffen Gebert <steffen.gebert@typo3.org>
2016-05-31 09:30:10 +0200

    Bump version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures status.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.5"
+version "0.1.0"
 
 depends "t3-base", "~> 0.2.0"
 
