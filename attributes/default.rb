7d18e7bf555f58f2840c17f510f473503038c780
Michael Stucki <michael.stucki@typo3.org>
2020-02-25 14:24:19 +0100

    Fix Git repository URL

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['site-statustypo3org']['fqdn']             = "status.typo3.org"
 default['site-statustypo3org']['docroot']          = "/var/www/vhosts/status.typo3.org/www"
-default['site-statustypo3org']['repository']       = "git://git.typo3.org/Sites/StatusTypo3Org.git"
+default['site-statustypo3org']['repository']       = "https://gitlab.typo3.org/services/status.typo3.org.git"
 default['site-statustypo3org']['api']['username']  = nil
 default['site-statustypo3org']['api']['password']  = nil
